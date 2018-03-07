a7439bf2b68c03ba1776f5c32ae82d1b380a3663
Anushka Fernando <anushka.rf@gmail.com>
2018-03-07 13:45:15 +1100

    Creation of Login Form

---
+++ src/pages/LoginForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { View } from "react-native";
+import CardSection from "./../components/CardSection";
+
+export default class LoginForm extends Component {
+  render() {
+    return (
+      <View>
+        <CardSection />
+      </View>
+    );
+  }
+}
